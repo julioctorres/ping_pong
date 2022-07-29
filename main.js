0411ffaf370aaf98a56f51fa2edc2ccd6a5aac9b
julioctorres <66503275+julioctorres@users.noreply.github.com>
2022-07-28 19:15:34 -0500

    Se agregan las barras de cada jugador

--- main.js
+++ main.js
@@ -82,12 +82,43 @@
 })();
 
 
-window.addEventListener("load", main);
 
-function main() {
-  var board = new Board(800, 400);
-  var canvas = document.getElementById("canvas");
-  var board_view = new BoardView(canvas, board);
-  
-   
+
+var board = new Board(800,400);
+var bar = new Bar(20,100,40,100,board);
+var bar_2 = new Bar(735,100,40,100,board);
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas,board);
+var ball = new Ball(350, 100, 10,board);
+
+document.addEventListener("keydown",function(ev){
+	
+	if(ev.keyCode == 38){
+		ev.preventDefault();
+		bar.up();
+	}
+	else if(ev.keyCode == 40){
+		ev.preventDefault();
+		bar.down();
+	}else if(ev.keyCode === 87){
+		ev.preventDefault();
+		//W
+		bar_2.up();
+	}else if(ev.keyCode === 83){
+		ev.preventDefault();
+		//S
+		bar_2.down();
+	}else if(ev.keyCode === 32){
+		ev.preventDefault();
+		board.playing = !board.playing;
+	}
+});
+
+board_view.draw();
+
+window.requestAnimationFrame(controller);
+
+function controller(){
+	board_view.play();
+	requestAnimationFrame(controller);
 }
